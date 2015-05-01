d8f0162c8bbcfba771259e9283534ee9d8dc7106
Leon Schönawa <lschoenawa@stud.hs-bremen.de>
2015-05-01 13:12:48 +0200

    store the components in arrays

--- src/main/java/de/hsbremen/battleshipextreme/client/ShipSelectionPanel.java
+++ src/main/java/de/hsbremen/battleshipextreme/client/ShipSelectionPanel.java
@@ -1,59 +1,91 @@
 package de.hsbremen.battleshipextreme.client;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
+import javax.swing.ButtonGroup;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class ShipSelectionPanel extends JPanel {
 	
+	JLabel[] lblShipCount;
+	JLabel[][] lblShips;
+	JRadioButton[] rbShipSelection;
+	
 	public ShipSelectionPanel() {
 		this.setLayout(new GridBagLayout());
-		this.setBorder(BorderFactory.createTitledBorder("Schiffauswahl"));
+		this.setBorder(BorderFactory.createTitledBorder("Your Ships"));
 		this.setBackground(Color.YELLOW);
 		
 		// GridBagLayout Einschränkungen
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.BOTH;
 		c.anchor = GridBagConstraints.NORTHWEST;
-		c.gridx = 0;
-		c.gridy = 0;
 		c.weightx = 0;
 		c.weighty = 0;
-		c.ipadx = c.ipady = 20;
-		c.insets = new Insets(0, 0, 1, 1);
+
+		// Labels für die Anzahl der Schiffe
+		this.lblShipCount = new JLabel[4];
 		
-		// Schiffe setzen
-		for (int y = 0; y < 4; y++) {
-			for (int x = 0; x < 5; x++) {
-				c.gridx = x;
-				c.gridy = y;
-				JLabel l = new JLabel();
-				l.setBackground(Color.BLUE);
-				l.setOpaque(true);
-				
-				if (y == 0)
-					this.add(l, c);
-				else if (y == 1 && x < 4)
-					this.add(l, c);
-				else if (y == 2 && x < 3)
-					this.add(l, c);
-				else if (y == 3 && x < 2)
-					this.add(l, c);
-			}
+		for (int i = 0; i < this.lblShipCount.length; i++) {
+			c.insets = new Insets(0, 3, 0, 5);
+			c.gridx = 0;
+			c.gridy = i;
+			this.lblShipCount[i] = new JLabel("0x");
+			this.lblShipCount[i].setFont(new Font("Tahoma", Font.BOLD, 15));
+			this.add(this.lblShipCount[i], c);
 		}
 		
-		// unsichtbares Label damit der Content links sitzt
-		c.gridx = 5;
-		c.gridy = 0;
-		c.weightx = 1;
-		c.weighty = 1;
-		c.ipadx = c.ipady = 0;
-		this.add(Box.createGlue(), c);
+		// Labels für die Schiffe
+		this.lblShips = new JLabel[4][];
+		this.lblShips[0] = new JLabel[5];
+		this.lblShips[1] = new JLabel[4];
+		this.lblShips[2] = new JLabel[3];
+		this.lblShips[3] = new JLabel[2];
+		
+		for (int y = 0; y < this.lblShips.length; y++) {
+			for (int x = 0; x < this.lblShips[y].length; x++) {
+				c.insets = new Insets(0, 0, 1, 1);
+				c.gridx = x + 1;
+				c.gridy = y;
+				c.ipadx = c.ipady = 30;
+				this.lblShips[y][x] = new JLabel();
+				this.lblShips[y][x].setBackground(Color.BLUE);
+				this.lblShips[y][x].setOpaque(true);
+				this.add(this.lblShips[y][x], c);
+			}
+		}
+
+		// RadioButtons fuer Schiffauswahl
+		c.ipadx = 0;
+		c.ipady = 0;
+		
+		this.rbShipSelection = new JRadioButton[4];
+		this.rbShipSelection[0] = new JRadioButton("Destroyer");
+		this.rbShipSelection[1] = new JRadioButton("Frigate");
+		this.rbShipSelection[2] = new JRadioButton("Corvette");
+		this.rbShipSelection[3] = new JRadioButton("Submarine");
+		
+		ButtonGroup group = new ButtonGroup();
+
+		for (int i = 0; i < this.rbShipSelection.length; i++) {
+			c.insets = new Insets(0, 5, 0, 0);
+			c.weightx = 1;
+			c.gridx = 6;
+			c.gridy = i;
+			this.rbShipSelection[i].setOpaque(false);
+			this.add(this.rbShipSelection[i], c);
+			group.add(this.rbShipSelection[i]);
+		}
+		
+		this.rbShipSelection[0].setSelected(true);
 	}
 }
