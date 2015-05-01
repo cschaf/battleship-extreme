f63de613c30de1825097e6feeaeb4f7ead58da34
Leon Schönawa <lschoenawa@stud.hs-bremen.de>
2015-05-01 18:06:43 +0200

    added panel for chat component

--- src/main/java/de/hsbremen/battleshipextreme/client/ChatPanel.java
+++ src/main/java/de/hsbremen/battleshipextreme/client/ChatPanel.java
@@ -11,30 +11,55 @@ import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class ChatPanel extends JPanel {
 	
-	JTextArea chatWindow;
+	private JTextArea chatWindowTextArea;
+	private JTextField chatMessageTextField;
+	private JButton sendMessageButton;
 	
 	public ChatPanel() {
 		this.setLayout(new GridBagLayout());
 		this.setBorder(BorderFactory.createTitledBorder("Chat"));
-		this.setBackground(Color.ORANGE);
 		
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.BOTH;
 		c.anchor = GridBagConstraints.NORTHWEST;
 
+		// Chat Window
 		c.gridx = 0;
 		c.gridy = 0;
 		c.weightx = 1;
 		c.weighty = 1;
-		this.chatWindow = new JTextArea("test");
-		this.chatWindow.setEditable(false);
-		JScrollPane scroll = new JScrollPane(this.chatWindow);
+		c.gridwidth = 2;
+		
+		chatWindowTextArea = new JTextArea();
+		chatWindowTextArea.setEditable(false);
+		chatWindowTextArea.setRows(6);
+		chatWindowTextArea.setLineWrap(true);
+		chatWindowTextArea.setWrapStyleWord(true);
+		
+		JScrollPane scroll = new JScrollPane(this.chatWindowTextArea);
 		this.add(scroll, c);
 		
+		// Chat Message
+		c.gridx = 0;
+		c.gridy = 1;
+		c.weightx = 1;
+		c.weighty = 1;
+		c.gridwidth = 1;
 		
+		chatMessageTextField = new JTextField();
+		this.add(chatMessageTextField, c);
 		
+		// Button
+		c.gridx = 1;
+		c.gridy = 1;
+		c.weightx = 0;
+		c.weighty = 0;
+		
+		sendMessageButton = new JButton("send");
+		this.add(sendMessageButton, c);
 	}
 }
