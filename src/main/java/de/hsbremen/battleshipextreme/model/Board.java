82ca6efd995692b928d63f69d98153aa7e778840
mueller-jan <mueller.jan.87@gmail.com>
2015-05-01 11:10:34 +0200

    -moved printBoard-method to Board class

--- src/main/java/de/hsbremen/battleshipextreme/model/Board.java
+++ src/main/java/de/hsbremen/battleshipextreme/model/Board.java
@@ -5,7 +5,7 @@ import de.hsbremen.battleshipextreme.model.exception.FieldOutOfBoardException;
 public class Board {
 	private Field[][] fields;
 	private int size;
-	
+
 	public Board(int size) {
 		this.size = size;
 		this.fields = new Field[size][size];
@@ -19,14 +19,51 @@ public class Board {
 	public Field[][] getFields() {
 		return fields;
 	}
-	
+
 	public Field getField(int x, int y) throws FieldOutOfBoardException {
 		if (x < 0 || y < 0 || x >= this.size || y >= this.size)
 			throw new FieldOutOfBoardException(new Field(x, y));
 		return this.fields[y][x];
 	}
-	
+
 	public int getSize() {
 		return size;
+	}
+
+	public void printBoard(boolean isOwnBoard) {
+		String s = isOwnBoard ? "\nEigenes Board" : "\nGegnerisches Board";
+		System.out.println(s);
+		Field[][] fields = this.getFields();
+		for (int row = 0; row < fields.length; row++) {
+			for (int column = 0; column < fields[row].length; column++) {
+				Field field = fields[row][column];
+				printState(field.getState(), isOwnBoard);
+			}
+			System.out.println();
+		}
+	}
+
+	private void printState(FieldState fieldState, boolean isOwnBoard) {
+		String s = "";
+		switch (fieldState) {
+		case Destroyed:
+			s = "!";
+			break;
+		case Hit:
+			s = "O";
+			break;
+		case Missed:
+			s = "X";
+			break;
+		case HasShip:
+			s = isOwnBoard ? "+" : "?";
+			break;
+		case IsEmpty:
+			s = isOwnBoard ? "-" : "?";
+			break;
+		default:
+			break;
+		}
+		System.out.print(s);
 	}
 }
