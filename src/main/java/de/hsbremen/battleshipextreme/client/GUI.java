46c9c33be5ef7348d5bd51cf5266382a73f3150e
Leon Schönawa <lschoenawa@stud.hs-bremen.de>
2015-05-04 17:38:37 +0200

    swapped single components to their own supercomponent class and added them to the gui

--- src/main/java/de/hsbremen/battleshipextreme/client/GUI.java
+++ src/main/java/de/hsbremen/battleshipextreme/client/GUI.java
@@ -17,80 +17,59 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 public class GUI {
-	JFrame frame;
-	ShipSelectionPanel panelShipSelection;
-	EnemySelectionPanel panelEnemySelection;
-	ChatPanel panelChat;
-	JPanel panelGame;
-	JLabel lblStatus;
-	JButton btnQuit;
+	private JFrame frame;
+	
+	private JMenuItem saveGameItem, loadGameItem, quitGameItem;
+	private JMenuItem manualItem;
+	
+	private GamePanel gamePanel;
 	
 	public GUI() {
-		this.frame = new JFrame("Battleship-Extreme");
-		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame = new JFrame("Battleship-Extreme");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setJMenuBar(createMenuBar());
 		
 		InitComponents();
 
-		this.frame.pack();
-		this.frame.setLocationRelativeTo(null);
-		this.frame.setVisible(true);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 	
 	private void InitComponents() {
+		gamePanel = new GamePanel();
+		frame.add(gamePanel);
+	}
+	
+	private JMenuBar createMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
 		
-		// Panel für das gesamte Spiel
-		this.panelGame = new JPanel(new BorderLayout());
-		this.frame.add(panelGame);
+		// Game Menue
+		JMenu gameMenu = new JMenu("Game");
+		menuBar.add(gameMenu);
 
-		// Panel für die Spieleinstelltungen (linke Seite)
-		JPanel panelSettings = new JPanel(new GridBagLayout());
-		this.panelGame.add(panelSettings, BorderLayout.WEST);
+		saveGameItem = new JMenuItem("Save...");
+		loadGameItem = new JMenuItem("Load...");
+		quitGameItem = new JMenuItem("Quit");
 		
-		// GridBagLayout Einschränkungen
-		GridBagConstraints c = new GridBagConstraints();
-		c.anchor = GridBagConstraints.NORTHWEST;
-		c.fill = GridBagConstraints.BOTH;
-		c.gridx = 0;
-		c.gridy = 0;
-		c.weightx = 1;
-		c.weighty = 0;
+		gameMenu.add(saveGameItem);
+		gameMenu.add(loadGameItem);
+		gameMenu.add(quitGameItem);
 		
-		// Label für die Statusanzeige
-		c.ipady = 30;
-		this.lblStatus = new JLabel("Informationsbereich!");
-		this.lblStatus.setBackground(Color.RED);
-		this.lblStatus.setFont(new Font("Tahoma", Font.BOLD, 20));
-		this.lblStatus.setOpaque(true);
-		panelSettings.add(lblStatus, c);
+		// Help Menue
+		JMenu helpMenu = new JMenu("Help");
+		menuBar.add(helpMenu);
 		
-		// Panel für die Schiffauswahl
-		c.gridy++;
-		c.ipady = 0;
-		this.panelShipSelection = new ShipSelectionPanel();
-		panelSettings.add(this.panelShipSelection, c);
+		manualItem = new JMenuItem("Manual");
+		helpMenu.add(manualItem);
 		
-		// Panel für die Gegnerauswahl
-		c.gridy++;
-		this.panelEnemySelection = new EnemySelectionPanel();
-		panelSettings.add(this.panelEnemySelection, c);
-
-		// Panel für die Gegnerauswahl
-		c.gridy++;
-		c.weighty = 1;
-		this.panelChat = new ChatPanel();
-		panelSettings.add(this.panelChat, c);
-		
-		// Button für die Aufgabe
-		c.anchor = GridBagConstraints.SOUTHWEST;
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridy++;
-		c.weighty = 0;
-		this.btnQuit = new JButton("Aufgeben");
-		panelSettings.add(btnQuit, c);
-		
+		return menuBar;
 	}
 }
