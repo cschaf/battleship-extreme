48cae98b3f4bbb15bcf68d2c214092f149728eaa
Leon Schönawa <lschoenawa@stud.hs-bremen.de>
2015-05-06 21:37:29 +0200

    added boards for player and enemy

--- src/main/java/de/hsbremen/battleshipextreme/client/GamePanel.java
+++ src/main/java/de/hsbremen/battleshipextreme/client/GamePanel.java
@@ -1,19 +1,54 @@
 package de.hsbremen.battleshipextreme.client;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class GamePanel extends JPanel {
 
-	GameplayPanel gameplayPanel;
+	private GameplayPanel gameplayPanel;
+	private JLabel infoLabel;
+	private BoardPanel enemyBoardPanel;
+	private BoardPanel playerBoardPanel;
 	
 	public GamePanel() {
+		
+		int size = 10;
+		
 		this.setLayout(new BorderLayout());
 		
 		// Panel fuer die Spielsteuerung
 		gameplayPanel = new GameplayPanel();
-		
 		this.add(gameplayPanel, BorderLayout.WEST);
+		
+		// Panel fuer die eigentliche Spielflaeche
+		JPanel gameAreaPanel = new JPanel(new BorderLayout());
+		this.add(gameAreaPanel);
+		
+		// Label fuer die Spielinformationen
+		infoLabel = new JLabel("Hier stehen aktuelle Spielinformationen!", SwingConstants.CENTER);
+		infoLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
+		infoLabel.setBackground(Color.orange);
+		infoLabel.setOpaque(true);
+		gameAreaPanel.add(infoLabel, BorderLayout.NORTH);
+		
+		// Spielbrett fuer den Gegner
+		enemyBoardPanel = new BoardPanel("Enemy", size);
+		gameAreaPanel.add(enemyBoardPanel, BorderLayout.WEST);
+
+		// Spielbrett fuer den Player
+		playerBoardPanel = new BoardPanel("Yours", size);
+		gameAreaPanel.add(playerBoardPanel, BorderLayout.EAST);
 	}
+	
+	public JLabel getInfoLabel() {
+		return infoLabel;
+	}
+	
+	
+	
 }
