522c85fa91fcd5043398eadf94303a27685b77f0
Leon Schönawa <lschoenawa@stud.hs-bremen.de>
2015-05-06 21:36:57 +0200

    implemented board panel

--- src/main/java/de/hsbremen/battleshipextreme/client/BoardPanel.java
+++ src/main/java/de/hsbremen/battleshipextreme/client/BoardPanel.java
@@ -1,7 +1,37 @@
 package de.hsbremen.battleshipextreme.client;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class BoardPanel extends JPanel {
-
+	
+	private JButton[][] fieldButtons;
+	
+	public BoardPanel(String title, int fieldSize) {
+		this.setBorder(BorderFactory.createTitledBorder(title));
+		this.setLayout(new GridLayout(fieldSize, fieldSize));
+		
+		fieldButtons = new JButton[fieldSize][fieldSize];
+		for (int y = 0; y < fieldButtons.length; y++) {
+			for (int x = 0; x < fieldButtons[y].length; x++) {
+				fieldButtons[y][x] = new JButton();
+				fieldButtons[y][x].setBackground(new Color(135, 206, 250));
+				fieldButtons[y][x].setPreferredSize(new Dimension(50, 50));
+				this.add(fieldButtons[y][x]);
+			}
+		}
+	}
+	
+	public JButton[][] getFieldButtons() {
+		return fieldButtons;
+	}
+	
+	public JButton getFieldButtonByIndex(int x, int y) {
+		return fieldButtons[y][x];
+	}
 }
