4ed410e1f3cb3bfe41d2512beae39dc8c28acc8f
mueller-jan <mueller.jan.87@gmail.com>
2015-06-29 10:29:18 +0200

    #11 added javadoc comments to board class

--- src/main/java/de/hsbremen/battleshipextreme/model/Board.java
+++ src/main/java/de/hsbremen/battleshipextreme/model/Board.java
@@ -10,6 +10,12 @@ public class Board implements Serializable {
 	private Field[][] fields;
 	private int size;
 
+	/**
+	 * Erzeugt ein Board anhand der übergebenen Größe.
+	 * 
+	 * @param size
+	 *            Boardgröße
+	 */
 	public Board(int size) {
 		this.size = size;
 		this.fields = new Field[size][size];
@@ -20,10 +26,25 @@ public class Board implements Serializable {
 		}
 	}
 
+	/**
+	 * Liefert alle Felder des Boards.
+	 * 
+	 * @return alle Felder
+	 */
 	public Field[][] getFields() {
 		return fields;
 	}
 
+	/**
+	 * Liefert ein bestimmtes Feld vom Board.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return das Feld an der Position x/y
+	 * @throws FieldOutOfBoardException
+	 *             wenn sich die Koordinaten nicht innerhalb des Feldes
+	 *             befinden.
+	 */
 	public Field getField(int x, int y) throws FieldOutOfBoardException {
 		if (!this.containsFieldAtPosition(x, y)) {
 			throw new FieldOutOfBoardException(new Field(x, y));
@@ -31,6 +52,14 @@ public class Board implements Serializable {
 		return this.fields[y][x];
 	}
 
+	/**
+	 * Dient zum Prüfen, ob das Board ein Feld mit den übergebenen Koordinaten
+	 * enthält.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return true, wenn das Feld enthalten ist, false wenn nicht
+	 */
 	public boolean containsFieldAtPosition(int x, int y) {
 		return (x < this.size) && (y < this.size) && (x >= 0) && (y >= 0);
 	}
@@ -39,13 +68,21 @@ public class Board implements Serializable {
 		return size;
 	}
 
+	/**
+	 * Liefert alle Feldzustände eines Boardes abhängig davon, ob es das eigene
+	 * Board oder das Board eines Gegners ist. Es werden nur Feldzustände
+	 * zurückgegeben, die der Spieler wissen darf.
+	 * 
+	 * @param isOwnBoard
+	 *            gibt an, ob es sich um das eigene Board handelt
+	 * @return
+	 */
 	public FieldState[][] getFieldStates(boolean isOwnBoard) {
 		FieldState[][] fieldStates = new FieldState[size][size];
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
 				FieldState state = fields[j][i].getState();
-				if ((state == FieldState.HAS_SHIP || state == FieldState.IS_EMPTY)
-						&& (!isOwnBoard)) {
+				if ((state == FieldState.HAS_SHIP || state == FieldState.IS_EMPTY) && (!isOwnBoard)) {
 					fieldStates[i][j] = null;
 				} else {
 					fieldStates[i][j] = state;
