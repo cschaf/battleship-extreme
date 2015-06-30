2e495f813f2468940d70c9e556b9ec068931fa64
Christian Schaf <cschaf@outlook.com>
2015-06-30 11:09:49 +0200

    added java docstring for ClientInfo issue #11

--- src/main/java/de/hsbremen/battleshipextreme/network/transfarableObject/ClientInfo.java
+++ src/main/java/de/hsbremen/battleshipextreme/network/transfarableObject/ClientInfo.java
@@ -5,11 +5,13 @@ import de.hsbremen.battleshipextreme.network.TransferableType;
 
 /**
  * Created by cschaf on 25.04.2015.
+ *
+ * Beinhaltet Informationen über einen Client
  */
 public class ClientInfo extends TransferInfo {
-    protected String ip;
-    protected int port;
-    private String username = null;
+    protected String ip; // IP-Adresse des Client
+    protected int port; // Port des Clients
+    private String username = null; // Name des Clients
 
     public ClientInfo(String username, String ip, int port) {
         super();
@@ -25,22 +27,42 @@ public class ClientInfo extends TransferInfo {
         this.username = username;
     }
 
+    /**
+     * Gibt den Benutzernamen des Clients zurück
+     * @return
+     */
     public String getUsername() {
         return this.username;
     }
 
+    /**
+     * Gibt die IP-Adresse des Clients zurück
+     * @return
+     */
     public String getIp() {
         return ip;
     }
 
+    /**
+     * Setzt die IP-Adresse des Clients
+     * @param ip
+     */
     public void setIp(String ip) {
         this.ip = ip;
     }
 
+    /**
+     * Gibt den Port des Clients zurück
+     * @return
+     */
     public int getPort() {
         return port;
     }
 
+    /**
+     * Setzt den Port des Clients
+     * @param port
+     */
     public void setPort(int port) {
         this.port = port;
     }
